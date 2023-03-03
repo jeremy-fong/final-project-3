adf604d6bae62f5eeea7566df98c903250ab3e8a
BuildTools <unconfigured@null.spigotmc.org>
2023-03-03 12:28:48 -0800

    added thread and user controllers. including comments for threads

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Signup = () => {
+  
+  return (
+      <>
+        <div>
+          <Header/>
+          HOME
+        </div>
+      </>
+  );
+};
+
+export default Signup;
