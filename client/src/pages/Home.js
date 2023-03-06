556baaa71003f54f6851549e9d2f4e9f333303e5
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-4.local>
2023-03-06 14:39:33 -0800

    light work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Home = () => {
   
   return (
-    <div className="card bg-white card-rounded w-50">
-      HOME
-    </div>
+   <div>
+      <nav></nav>
+
+   </div>
   );
 };
 
