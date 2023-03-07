083b18853710a6935feaa462fa60ae668f750caf
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-4.local>
2023-03-06 16:27:31 -0800

    typedefs/user model work on server side

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -21,11 +21,13 @@ const typeDefs = gql`
    }
 
    type User {
-    _id: ID
-    username: String
-    email: String
-    threads: [Thread]
-    followers: [User]
-   }
+      _id: ID
+      firstName: String
+      lastName: String
+      email: String
+      threads: [Thread]
+      followers: [User]
+    }
+
 `
 module.exports = typeDefs
