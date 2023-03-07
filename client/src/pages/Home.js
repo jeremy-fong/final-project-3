4ee8f0798b7d176ce405069980c70ac7acab4785
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-4.local>
2023-03-06 16:19:23 -0800

    light work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,19 +4,10 @@ import Header from '../components/Header';
 const Home = () => {
   
   return (
-<<<<<<< HEAD
    <div>
       <nav></nav>
 
    </div>
-=======
-      <>
-        <div>
-          <Header/>
-          HOME
-        </div>
-      </>
->>>>>>> 7ddb09b45e46358554823b39be0e0e7e7f022482
   );
 };
 
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,6 +1,31 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-   
+   type Category {
+    _id: ID 
+    categoryTitle: String
+   }
+
+   type Comment {
+    _id: ID
+    commentBody: String
+    username: String
+    createdAt: Date
+   }
+
+   type Thread {
+    _id: ID
+    threadText: String
+    createdAt: Date
+    username: String
+   }
+
+   type User {
+    _id: ID
+    username: String
+    email: String
+    threads: [Thread]
+    followers: [User]
+   }
 `
 module.exports = typeDefs
