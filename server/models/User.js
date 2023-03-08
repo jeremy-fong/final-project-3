d5f160c4fdc91592fcabc137eae73c8630e61033
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-6.local>
2023-03-07 20:02:05 -0800

    signup /login

--- server/models/User.js
+++ server/models/User.js
@@ -11,16 +11,6 @@ const userSchema = new Schema({
         trim: true
     },
 
-  firstName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true
-  },
   email: {
     type: String,
     required: true,
