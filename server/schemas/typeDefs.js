4add24c1a23e70fe29bb4631b7f282d549b77bc2
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 10:16:34 -0800

    fixed typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,18 +15,17 @@ const typeDefs = gql`
 
    type Thread {
     _id: ID
-    title: String
-    body: String
-    createdAt: String
     username: String
+    title: String
+    description: String
    }
 
    type User {
       _id: ID
       username: String
       email: String
-      threads: [Thread]
-      followerCount: Int
+      picturePath: String
+      followers: Int
    }
 
    type Auth {
@@ -39,16 +38,15 @@ const typeDefs = gql`
    }
 
    input threadInput {
-      creator: String
-      threadId: String
       title: String
-      text: String
+      description: String
+      threadId: String
    }
 
    type Mutation {
       login(email: String!, password: String!): Auth
       addUser(username: String!, email: String!, password: String!): Auth
-      createThread(thread: threadInput): User
+      createThread(thread: threadIput): User
       removeThread(threadId: String!): User
    }
 
